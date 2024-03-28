1ead24995e47ed7993a8285d9113372ca43fb9ac
Eugeny Zhikharev <janiqgames@gmail.com>
2024-03-28 17:15:49 +0700

    moveing parsing files to separate file and function

---
+++ src/Parser.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Parser;
+
+use Symfony\Component\Yaml\Yaml;
+
+function parseFile(string $file)
+{
+    $fileData = (string) file_get_contents($file, true);
+    $extension = pathinfo($file, PATHINFO_EXTENSION);
+    // var_dump($extension);
+
+    if ($extension === 'json') {
+        return json_decode($fileData);
+    }
+    if ($extension === 'yml' || $extension === 'yaml') {
+        return Yaml::parse($fileData, Yaml::PARSE_OBJECT_FOR_MAP);
+    }
+}
