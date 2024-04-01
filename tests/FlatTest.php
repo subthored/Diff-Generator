40023352ec176a1be49e1571fa8c7835640c760b
Eugeny Zhikharev <janiqgames@gmail.com>
2024-04-01 14:26:57 +0700

    rewrite tests with tree data

--- tests/FlatTest.php
+++ tests/FlatTest.php
@@ -11,12 +11,48 @@ class FlatTest extends Testcase
     public function testFlatData(): void
     {
         $expected = "{
-- follow: false
-host: hexlet.io
-- proxy: 123.234.53.22
-- timeout: 50
-+ timeout: 20
-+ verbose: true
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
 }";
         $fileJson1 = 'tests/fixtures/file1.json';
         $fileJson2 = 'tests/fixtures/file2.json';
