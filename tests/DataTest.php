301575f31e66eed2aae16f4d962bdced0825fe70
Eugeny Zhikharev <janiqgames@gmail.com>
2024-04-03 13:26:18 +0700

    rewrite tests with asserting file to string

--- tests/DataTest.php
+++ tests/DataTest.php
@@ -8,82 +8,39 @@ use function src\genDiff;
 
 class DataTest extends Testcase
 {
-    public function testFlatData(): void
+    public function testStylishFormat(): void
     {
-        $expected = "{
-    common: {
-      + follow: false
-        setting1: Value 1
-      - setting2: 200
-      - setting3: true
-      + setting3: null
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-        }
-        setting6: {
-            doge: {
-              - wow: 
-              + wow: so much
-            }
-            key: value
-          + ops: vops
-        }
-    }
-    group1: {
-      - baz: bas
-      + baz: bars
-        foo: bar
-      - nest: {
-            key: value
-        }
-      + nest: str
-    }
-  - group2: {
-        abc: 12345
-        deep: {
-            id: 45
-        }
-    }
-  + group3: {
-        deep: {
-            id: {
-                number: 45
-            }
-        }
-        fee: 100500
-    }
-}";
+        $expected = __DIR__ . '/fixtures/expStylish.txt';
         $fileJson1 = 'tests/fixtures/file1.json';
         $fileJson2 = 'tests/fixtures/file2.json';
         $fileYaml1 = 'tests/fixtures/file1.yaml';
         $fileYaml2 = 'tests/fixtures/file2.yaml';
 
-        $this->assertEquals($expected, genDiff($fileJson1, $fileJson2));
-        $this->assertEquals($expected, genDiff($fileYaml1, $fileYaml2));
+        $this->assertStringEqualsFile($expected, genDiff($fileJson1, $fileJson2));
+        $this->assertStringEqualsFile($expected, genDiff($fileYaml1, $fileYaml2));
     }
 
     public function testPlainFormat(): void
     {
-        $expected = "Property 'common.follow' was added with value: false
-Property 'common.setting2' was removed
-Property 'common.setting3' was updated. From true to null
-Property 'common.setting4' was added with value: 'blah blah'
-Property 'common.setting5' was added with value: [complex value]
-Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
-Property 'common.setting6.ops' was added with value: 'vops'
-Property 'group1.baz' was updated. From 'bas' to 'bars'
-Property 'group1.nest' was updated. From [complex value] to 'str'
-Property 'group2' was removed
-Property 'group3' was added with value: [complex value]
-";
-
+        $expected = __DIR__ . '/fixtures/expStylish.txt';
         $fileJson1 = 'tests/fixtures/file1.json';
         $fileJson2 = 'tests/fixtures/file2.json';
         $fileYaml1 = 'tests/fixtures/file1.yaml';
         $fileYaml2 = 'tests/fixtures/file2.yaml';
 
-        $this->assertEquals($expected, genDiff($fileJson1, $fileJson2, 'plain'));
-        $this->assertEquals($expected, genDiff($fileYaml1, $fileYaml2, 'plain'));
+        $this->assertStringEqualsFile($expected, genDiff($fileJson1, $fileJson2));
+        $this->assertStringEqualsFile($expected, genDiff($fileYaml1, $fileYaml2));
+    }
+
+    public function testJsonFormat(): void
+    {
+        $expected = __DIR__ . '/fixtures/expStylish.txt';
+        $fileJson1 = 'tests/fixtures/file1.json';
+        $fileJson2 = 'tests/fixtures/file2.json';
+        $fileYaml1 = 'tests/fixtures/file1.yaml';
+        $fileYaml2 = 'tests/fixtures/file2.yaml';
+
+        $this->assertStringEqualsFile($expected, genDiff($fileJson1, $fileJson2));
+        $this->assertStringEqualsFile($expected, genDiff($fileYaml1, $fileYaml2));
     }
 }
