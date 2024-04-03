91b0fc7342b729ae2f79e02de786299c7a727c23
Eugeny Zhikharev <janiqgames@gmail.com>
2024-04-03 15:14:00 +0700

    using match instead of if statement

--- src/Parser.php
+++ src/Parser.php
@@ -6,21 +6,23 @@ use Symfony\Component\Yaml\Yaml;
 
 function parseFile(string $file): object
 {
-    if (!file_exists($file)) {
-        throw new \Exception('No such file!');
-    }
+    // if (!file_exists($file)) {
+    //     throw new \Exception('No such file!');
+    // }
 
     $fileData = (string) file_get_contents($file, true);
     $extension = pathinfo($file, PATHINFO_EXTENSION);
-    $returnData = [];
     // var_dump($extension);
 
-    if ($extension === 'json') {
-        $returnData = json_decode($fileData);
-    }
-    if ($extension === 'yml' || $extension === 'yaml') {
-        $returnData = Yaml::parse($fileData, Yaml::PARSE_OBJECT_FOR_MAP);
-    }
-
-    return $returnData;
+    return match ($extension) {
+        'json' => json_decode($fileData),
+        'yml', 'yaml' => Yaml::parse($fileData, Yaml::PARSE_OBJECT_FOR_MAP),
+        default => throw new \Exception('No such file!'),
+    };
+    // if ($extension === 'json') {
+    //     return json_decode($fileData);
+    // }
+    // if ($extension === 'yml' || $extension === 'yaml') {
+    //     return Yaml::parse($fileData, Yaml::PARSE_OBJECT_FOR_MAP);
+    // }
 }
