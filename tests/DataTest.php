d483ca04bb018b88ed5aa1c15eef83a423e6f865
Eugeny Zhikharev <janiqgames@gmail.com>
2024-04-03 12:38:40 +0700

    renamed and added new tests

--- tests/DataTest.php
+++ tests/DataTest.php
@@ -6,7 +6,7 @@ use PHPUnit\Framework\TestCase;
 
 use function src\genDiff;
 
-class FlatTest extends Testcase
+class DataTest extends Testcase
 {
     public function testFlatData(): void
     {
@@ -61,5 +61,29 @@ class FlatTest extends Testcase
 
         $this->assertEquals($expected, genDiff($fileJson1, $fileJson2));
         $this->assertEquals($expected, genDiff($fileYaml1, $fileYaml2));
+    }
+
+    public function testPlainFormat(): void
+    {
+        $expected = "Property 'common.follow' was added with value: false
+Property 'common.setting2' was removed
+Property 'common.setting3' was updated. From true to null
+Property 'common.setting4' was added with value: 'blah blah'
+Property 'common.setting5' was added with value: [complex value]
+Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
+Property 'common.setting6.ops' was added with value: 'vops'
+Property 'group1.baz' was updated. From 'bas' to 'bars'
+Property 'group1.nest' was updated. From [complex value] to 'str'
+Property 'group2' was removed
+Property 'group3' was added with value: [complex value]
+";
+
+        $fileJson1 = 'tests/fixtures/file1.json';
+        $fileJson2 = 'tests/fixtures/file2.json';
+        $fileYaml1 = 'tests/fixtures/file1.yaml';
+        $fileYaml2 = 'tests/fixtures/file2.yaml';
+
+        $this->assertEquals($expected, genDiff($fileJson1, $fileJson2, 'plain'));
+        $this->assertEquals($expected, genDiff($fileYaml1, $fileYaml2, 'plain'));
     }
 }
