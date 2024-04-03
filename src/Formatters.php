95530e3f3586c062c453656c80fffee32e02354f
Eugeny Zhikharev <janiqgames@gmail.com>
2024-04-03 13:26:52 +0700

    added json formatter

--- src/Formatters.php
+++ src/Formatters.php
@@ -4,8 +4,9 @@ namespace format;
 
 use function stylishRender\renderStylish;
 use function plainRender\renderPlain;
+use function jsonRender\renderJson;
 
-function format(array $array, string $formatter)
+function format(array $array, string $formatter): string
 {
     if ($formatter === 'stylish') {
         return renderStylish($array);
@@ -13,5 +14,9 @@ function format(array $array, string $formatter)
 
     if ($formatter === 'plain') {
         return renderPlain($array, $valuePath = "");
+    }
+
+    if ($formatter === 'json') {
+        return renderJson($array);
     }
 }
